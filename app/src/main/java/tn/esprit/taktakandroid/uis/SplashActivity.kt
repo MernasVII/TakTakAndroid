b764b914ef468e80f31072862b2184b5b94b41e0
MeriamBi <meriambenida@gmail.comgit config --global user.name MeriamBigit config --global user.email meriambenida@gmail.com>
2023-03-15 19:30:33 +0100

    initial commit

---
+++ app/src/main/java/tn/esprit/taktakandroid/uis/SplashActivity.kt
@@ -0,0 +1,12 @@
+package tn.esprit.taktakandroid.uis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tn.esprit.taktakandroid.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
